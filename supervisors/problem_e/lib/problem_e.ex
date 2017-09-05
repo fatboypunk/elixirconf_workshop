8d83a4c358d876f949f94985b686f0aa81dd9477
Marcel Horlings <marcel.horlings@gmail.com>
2017-09-05 15:19:16 -0700

    solve problem e

--- supervisors/problem_e/lib/problem_e.ex
+++ supervisors/problem_e/lib/problem_e.ex
@@ -6,20 +6,34 @@ defmodule ProblemE do
   @doc """
   Start a Supervisor for Agents.
   """
-  def start_link(opts \\ [])
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, opts, opts)
+  end
 
   @doc """
   Initialize the Supervisor
   """
-  def init(opts)
+  def init(opts) do
+    {shutdown, opts} = Keyword.pop(opts, :shutdown, 5000)
+    {restart,opts} = Keyword.pop(opts, :restart, :temporary)
+    agent = %{id: Agent,
+      start: {Agent, :start_link, []},
+      shutdown: shutdown,
+      restart: restart}
+      Supervisor.init([agent], [strategy: :simple_one_for_one] ++ opts)
+  end
 
   @doc """
   Start an Agent with a fun.
   """
-  def start_child(sup, fun, opts \\ [])
+  def start_child(sup, fun, opts \\ []) do
+    Supervisor.start_child(sup, [fun, opts])
+  end
 
   @doc """
   Start an Agent with module, function and arguments.
   """
-  def start_child(sup, mod, fun, args, opts \\ [])
+  def start_child(sup, mod, fun, args, opts \\ []) do
+    Supervisor.start_child(sup, [mod, fun, args, opts])
+  end
 end
